8057f74118c626d3fbe836e917f7543238f23894
housni.h <abdel.housni@gmail.com>
2019-04-05 12:40:30 +0200

    Update macos-OSX-scripts.sh

--- macos-OSX-scripts.sh
+++ macos-OSX-scripts.sh
@@ -1,1 +1,14 @@
+# exemples de scripts utiles pour macos-OSX
+
+
+# Activer bash-completion
+# source : https://www.simplified.guide/macos/bash-completion
+
+# Liste des services/daemon
+sudo launchctl list
+
+# stop service
+sudo launchctl stop service
+
+
 
