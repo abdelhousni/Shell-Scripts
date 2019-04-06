7fc3499b48dbaa6fb6ed2e76025bd0f569653951
housni.h <abdel.housni@gmail.com>
2019-04-06 17:55:07 +0200

    Update dpkg.sh

--- dpkg.sh
+++ dpkg.sh
@@ -7,6 +7,7 @@ find /var/lib/dpkg/info/ -name *.list -exec stat -c $'%y\t %n' {} \;
 # List installaed packages by installation date
 # source : https://unix.stackexchange.com/questions/12578/list-packages-on-an-apt-based-system-by-installation-date
 
+# Methode 1
 # build list of packages by dates
 $ find /var/lib/dpkg/info -name "*.list" -exec stat -c $'%n\t%y' {} \; | \
     sed -e 's,/var/lib/dpkg/info/,,' -e 's,\.list\t,\t,' | \
@@ -18,3 +19,7 @@ $ dpkg --get-selections | sed -ne '/\tinstall$/{s/[[:space:]].*//;p}' | \
 # join the 2 lists
 $ join -1 1 -2 1 -t $'\t' ~/dpkglist.selections ~/dpkglist.dates \
     > ~/dpkglist.selectiondates
+
+# Methode 2
+$ zgrep 'install ' /var/log/dpkg.log.1 | sort | cut -f1,2,4 -d' '
+
