6809601bdf0a3097754fc634174df88eca536bf7
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2019-05-05 20:09:18 +0200

    Update bash-scripts.sh

--- bash-scripts.sh
+++ bash-scripts.sh
@@ -2,3 +2,7 @@
 
 # remove files (in current path) with size 0
 find . -size 0 -delete
+
+# rename all file in folder using ls | xargs | cp
+# source : https://unix.stackexchange.com/questions/349252/cp-after-xargs-not-working
+ls /source/path/*pattern* | xargs -I{} cp -u {} /destination/path
