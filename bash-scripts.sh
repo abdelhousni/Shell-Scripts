f5f8c3c088caddbf765798ab24f148251c306fd6
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2019-08-07 16:26:49 +0200

    Update bash-scripts.sh

--- bash-scripts.sh
+++ bash-scripts.sh
@@ -6,3 +6,7 @@ find . -size 0 -delete
 # rename all file in folder using ls | xargs | cp
 # source : https://unix.stackexchange.com/questions/349252/cp-after-xargs-not-working
 ls /source/path/*pattern* | xargs -I{} cp -u {} /destination/path
+
+# download all folder files (source forge)
+# source : https://stackoverflow.com/questions/39668291/download-whole-folder-from-sourceforge
+curl https://sourceforge.net/projects/broll/rss?path=/dicts-babylon/001 | grep "<link>.*</link>" | sed 's|<link>||;s|</link>||' | while read url; do url=`echo $url | sed 's|/download$||'`; wget $url ; done
